c9008a4c54aac200839427a3f7014eeafd941a31
kpal57484 <amiteshkriti007@gmail.com>
2016-10-22 16:37:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
 var element=document.getElementById('main_text');
 element.innerHTML='let us go';
+
+//move the image
+var madi=document.getElementById('madi');
+madi.onClick=function(){
+    madi.style.marginLeft='100px';
+};
